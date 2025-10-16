b09e099e045b1c8556bf42aa776602270ab912d7
hanibasha3270 <hanibasha3270@gmail.com>
2025-10-16 09:42:27 +0200

    An array

---
+++ index.js
@@ -0,0 +1,26 @@
+//what is an array in js ?
+//an array in js is adata strucute used to store multiple values in one varebale
+
+
+// Q2
+// how do we creat an array?
+// Ex
+let fruits = [`apple , bnana , mango`]
+
+let fruit = [];
+fruit [0]= `apple`;
+fruit [1]=  `bnana`;
+
+//Q3
+//what data tyeps can you store in an array ?
+ // name & number
+ let mixed = ["hani" ,24,"true" ]
+
+
+ //Q4
+ // what is the diffrenete between an array and asigle variable ?
+    let name = `hani`
+    let names= [`hani,Rehab,Ali`]
+
+//Q5 
+//how do you access spesific items from an array ?
