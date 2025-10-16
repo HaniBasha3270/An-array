af7fc7d445dc40702674ed14faf936afb82c5222
hanibasha3270 <hanibasha3270@gmail.com>
2025-10-16 10:01:37 +0200

    array

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // Q2
 // how do we creat an array?
 // Ex
-let fruits = [`apple , bnana , mango`]
+let fruits = [`apple , bnana , mango`];
 
 let fruit = [];
 fruit [0]= `apple`;
@@ -14,13 +14,31 @@ fruit [1]=  `bnana`;
 //Q3
 //what data tyeps can you store in an array ?
  // name & number
- let mixed = ["hani" ,24,"true" ]
+ let mixed = ["hani" ,24,"true" ];
 
 
  //Q4
  // what is the diffrenete between an array and asigle variable ?
-    let name = `hani`
-    let names= [`hani,Rehab,Ali`]
+    let name = `hani`;
 
 //Q5 
 //how do you access spesific items from an array ?
+let colors = [`red , green , blue`];
+console.log(colors[0]);
+console.log(colors[ colors.length-1]);
+
+//Q6 
+// how do you change avalue in an array ?
+let fruit=[`apple , bnana , mango`];
+fruit[1] = `orange`;
+console.log(fruit);
+
+//Q7
+//  what dose length do ?
+// it give us number of itmes insaid an array .
+let animale = [`loin , donke , monke`]
+console.log(animale.length)
+ 
+//Q8
+//how can you add a new element in an array
+
